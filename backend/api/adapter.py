7ca4c7e8134ac229ad8a1bcb423bca2f3758b756
Tran Chi Nghi <trannghi1672004@gmail.com>
2026-05-29 21:00:20 +0700

    sửa logic connect lại nếu thêm email thì khi login 1 acc dùng đc nhiều tài khoản google login

--- backend/api/adapter.py
+++ backend/api/adapter.py
@@ -11,21 +11,23 @@ class MySocialAccountAdapter(DefaultSocialAccountAdapter): #chỉnh sủa login 
         if request.user.is_authenticated:
             return
 
+        # nếu social account đã tồn tại rồi thì return luôn, ví dụ user có 2 mail a và b thì đều gắn vào user đó không tạo acc mới
+        if sociallogin.is_existing: # chạy trước hàm này và tự động query, ví dụ gmail a đã connect và đc tạo trc đây thì return k cần phải connect lại
+            return
+
         email = sociallogin.account.extra_data.get('email') # lấy ra email vừa login google
         if not email:
             return
 
         try:
-            # Tìm user theo email login google
-            user = User.objects.get(email=email)
 
             # Kiểm tra email đã xác minh chưa (được gửi từ allauth)
-            email_obj = EmailAddress.objects.filter(user=user, email=email, verified=True).first()
+            email_obj = EmailAddress.objects.filter(email=email, verified=True).select_related("user").first()
             if email_obj:
                 # Nếu đã xác minh, gắn social login vào user hiện có
-                sociallogin.connect(request, user)
+                sociallogin.connect(request, email_obj.user)
 
-        except User.DoesNotExist:
+        except User.DoesNotExist: #tạo account ggoogle mới
             pass
 
     # đồng bộ tên email là tên ng dùng khi đăng nhập google
